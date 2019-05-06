52f432d51c721de8974fa77c8dcddf4c918b0f00
kazugufaith <faithmugesia@gmail.com>
2019-05-06 17:57:03 +0300

    link bootstrap in styles.css and add background-color on styles.css file

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+// import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+
+// import { SService } from './s.service';
+// import { NO_ERRORS_SCHEMA } from '@angular/core';
+
+
+// import { AppComponent } from './app.component';
+// import { AppRoutingModule } from './app-routing/app-routing.module';
+// import { HeaderComponent } from './header/header.component';
+// import { ReposComponent } from './repos/repos.component';
+// import { UserComponent } from './user/user.component';
+// import { FormsModule } from '@angular/forms';
+// import { HttpClientModule } from '@angular/common/http';
+
+// @NgModule({
+//     declarations: [
+//         AppComponent,
+//         HeaderComponent,
+//         ReposComponent,
+//         UserComponent,
+//     ],
+//     imports: [
+//         BrowserModule,
+//         AppRoutingModule,
+//         FormsModule,
+//         HttpClientModule
+//     ],
+//     schemas: [NO_ERRORS_SCHEMA],
+//     providers: [SService],
+//     bootstrap: [AppComponent]
+// })
+// export class AppModule { }
