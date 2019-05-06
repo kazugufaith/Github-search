cbdb51bae233dc7b46dd062fca662221ec979703
kazugufaith <faithmugesia@gmail.com>
2019-05-06 18:04:50 +0300

    added functions on services.ts file

--- src/app/s.service.ts
+++ src/app/s.service.ts
@@ -1,31 +1,31 @@
-// import { Injectable } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-// @Injectable()
-// export class SService {
-//     private username: string;
-//     private clientid = '1895a42d6e4fb55798ad';
-//     private clientsecret = 'fa73a965ca5d9ea680ec7af4ff0d83b40c634a85';
+@Injectable()
+export class SService {
+    private username: string;
+    private clientid = '1895a42d6e4fb55798ad';
+    private clientsecret = 'fa73a965ca5d9ea680ec7af4ff0d83b40c634a85';
 
-//     constructor(private http: HttpClient) {
-//         console.log('Ready for display');
-//         this.username = 'kazugufaith';
-//     }
-//     getprofileInfo(username) {
-//         console.log(this.username);
+    constructor(private http: HttpClient) {
+        console.log('Ready for display');
+        this.username = 'kazugufaith';
+    }
+    getprofileInfo(username) {
+        console.log(this.username);
     
-//         return this.http.get('https://api.github.com/users/' + this.username + '?client_id=' + this.clientid + '&client_secret=' + this.clientsecret);
+        return this.http.get('https://api.github.com/users/' + this.username + '?client_id=' + this.clientid + '&client_secret=' + this.clientsecret);
 
-//     }
-//     getprofileRepos() {
+    }
+    getprofileRepos() {
 
-//         return this.http.get('https://api.github.com/users/' + this.username + '/repos?client_id=' + this.clientid + '&client_secret=' + this.clientsecret);
+        return this.http.get('https://api.github.com/users/' + this.username + '/repos?client_id=' + this.clientid + '&client_secret=' + this.clientsecret);
 
 
-//     }
+    }
 
-//     updateprofile(username: string) {
-//         this.username = username;
-//     }
+    updateprofile(username: string) {
+        this.username = username;
+    }
 
-// }
+}
